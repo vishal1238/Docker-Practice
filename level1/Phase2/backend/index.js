3c47e6689e438fc5577ccb5690c737540706bb9d
vishal1238 <webervishal@gmail.com>
2026-07-08 15:31:10 +0530

    updated code

---
+++ level1/Phase2/backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+console.log("PORT =", port);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from Docker Phase2" });
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Listening on ${port}`);
+});
